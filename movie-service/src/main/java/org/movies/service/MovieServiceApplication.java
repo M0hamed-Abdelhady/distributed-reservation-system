52c7b0e95306cd699861e2fc97467c3cc737f913
Mohamed Abdelhady <mohamedabdelhady.official@gmail.com>
2025-03-19 03:38:56 +0200

    Added: movie service

---
+++ movie-service/src/main/java/org/movies/service/MovieServiceApplication.java
@@ -0,0 +1,7 @@
+package org.movies.service;
+
+public class MovieServiceApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ movie-service/src/main/java/org/movies/service/dtos/Response.java
@@ -0,0 +1,16 @@
+package org.movies.service.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Response {
+    private String status;
+    private String message;
+    private Object data;
+}
